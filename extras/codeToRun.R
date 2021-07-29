940af78dc47c3af32b55c86441d83d20eabf0679
Sebastien Cossin <sebastien.cossin@chu-bordeaux.fr>
2021-07-29 09:59:53 +0200

    codeToRun at Bordeaux University Hospital

--- extras/codeToRun.R
+++ extras/codeToRun.R
@@ -1,17 +1,12 @@
-
-
+source("./getConnectionDetails.R")
 
 # fill out the connection details -----------------------------------------------------------------------
-connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "", user = "", 
-                                                                password = "", server = "", 
-                                                                port = "", extraSettings = "")
-
-cdmDatabaseSchema <- "yourCdmSchema" # the fully qualified database schema name of the CDM
-resultsDatabaseSchema <- "yourResultsSchema" # the fully qualified database schema name of the results schema (that you can write to)
-cdmSourceName <- "Your CDM Source" # a human readable name for your CDM source
+cdmDatabaseSchema <- "OMOP" # the fully qualified database schema name of the CDM
+resultsDatabaseSchema <- "OMOP_COHORT" # the fully qualified database schema name of the results schema (that you can write to)
+cdmSourceName <- "CDWBordeaux" # a human readable name for your CDM source
 
 # determine how many threads (concurrent SQL sessions) to use ----------------------------------------
-numThreads <- 3 # on Redshift, 3 seems to work well
+numThreads <- 1 # on Redshift, 3 seems to work well
 
 # specify if you want to execute the queries or inspect them ------------------------------------------
 sqlOnly <- FALSE # set to TRUE if you just want to get the SQL scripts and not actually run the queries
@@ -20,7 +15,7 @@ sqlOnly <- FALSE # set to TRUE if you just want to get the SQL scripts and not a
 outputFolder <- "output"
 
 # logging type -------------------------------------------------------------------------------------
-verboseMode <- FALSE # set to TRUE if you want to see activity written to the console
+verboseMode <- T # set to TRUE if you want to see activity written to the console
 
 # write results to table? -----------------------------------------------------------------------
 writeToTable <- FALSE # set to FALSE if you want to skip writing to results table
@@ -43,9 +38,17 @@ checkLevels <- c("TABLE", "FIELD", "CONCEPT")
 checkNames <- c() #Names can be found in inst/csv/OMOP_CDM_v5.3.1_Check_Desciptions.csv
 
 # which CDM tables to exclude? ------------------------------------
+emptyTables <- c("OBSERVATION", 
+                 "DRUG_ERA",
+                 "DOSE_ERA",
+                 "DEVICE_EXPOSURE", 
+                 "NOTE_NLP", 
+                 "SPECIMEN",  
+                 "FACT_RELATIONSHIP",
+                 "PAYER_PLAN_PERIOD",
+                 "COST")
 
-tablesToExclude <- c() 
-
+tablesToExclude <- c(emptyTables)
 # run the job --------------------------------------------------------------------------------------
 DataQualityDashboard::executeDqChecks(connectionDetails = connectionDetails, 
                               cdmDatabaseSchema = cdmDatabaseSchema, 
@@ -61,7 +64,7 @@ DataQualityDashboard::executeDqChecks(connectionDetails = connectionDetails,
                               checkNames = checkNames)
 
 # inspect logs ----------------------------------------------------------------------------
-ParallelLogger::launchLogViewer(logFileName = file.path(outputFolder, cdmSourceName, 
+ParallelLogger::launchLogViewer(logFileName = file.path(outputFolder, 
                                                         sprintf("log_DqDashboard_%s.txt", cdmSourceName)))
 
 # (OPTIONAL) if you want to write the JSON file to the results table separately -----------------------------
@@ -70,4 +73,6 @@ DataQualityDashboard::writeJsonResultsToTable(connectionDetails = connectionDeta
                                               resultsDatabaseSchema = resultsDatabaseSchema, 
                                               jsonFilePath = jsonFilePath)
 
-DataQualityDashboard::viewDqDashboard(file.path(getwd(),outputFolder, cdmSourceName, paste0("results_", cdmSourceName, ".json")))
+jsonFile <- "CDWbordeaux-20210728232540.json"
+DataQualityDashboard::viewDqDashboard(file.path(getwd(),outputFolder, 
+                                                jsonFile))
